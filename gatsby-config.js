860bd387ddcda4927f009166f88f022b3341aeb8
Grace Zheng <gzheng77@gmail.com>
2021-04-01 15:16:44 -0500

    gatsby to gh-pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,13 +1,16 @@
 const config = require('./src/config');
 
 module.exports = {
+  pathPrefix: '/v4/home',
+};
+
+module.exports = {
   siteMetadata: {
-    title: 'Brittany Chiang',
-    description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+    title: 'Matthew Taruno',
+    description: 'Matthew Taruno is a data scientist.',
+    siteUrl: 'https://mtaruno.github.io/v4', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
+    twitterUsername: '',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -19,8 +22,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'BrittanyChiang',
-        short_name: 'BrittanyChiang',
+        name: 'MatthewTaruno',
+        short_name: 'MatthewTaruno',
         start_url: '/',
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
